6451d6a0e609f596f4f31a0878f963b8791743c3
lidong <dongli72@gmail.com>
2026-04-01 15:45:24 +0800

    init

---
+++ src/main/java/free/servpp/cbodjava/CbodjavaApplication.java
@@ -0,0 +1,13 @@
+package free.servpp.cbodjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbodjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CbodjavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/free/servpp/cbodjava/CbodjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package free.servpp.cbodjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbodjavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
