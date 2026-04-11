7ef29ed86a190d55fe9dab933b0b1dfc02e7a68b
lidong <lidong@example.com>
2026-04-11 12:15:57 +0800

    Enable scanning generated CICS controllers

--- src/main/java/free/servpp/cbodjava/CbodjavaApplication.java
+++ src/main/java/free/servpp/cbodjava/CbodjavaApplication.java
@@ -3,7 +3,7 @@ package free.servpp.cbodjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"free.servpp.cbodjava", "cbod.java"})
 public class CbodjavaApplication {
 
     public static void main(String[] args) {
